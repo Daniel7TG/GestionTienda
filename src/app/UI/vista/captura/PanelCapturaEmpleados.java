8b99793ea7e4db3581878961e6212606002e5a8a
Daniel7TG <odtgodtg000@gmail.com>
2024-05-04 15:51:24 -0600

    Inicio de vista Usuarios

---
+++ src/app/UI/vista/captura/PanelCapturaEmpleados.java
@@ -0,0 +1,22 @@
+package app.UI.vista.captura;
+
+import java.awt.Component;
+
+import javax.swing.JPanel;
+
+import app.interfaces.Service;
+import app.modelos.Usuario;
+
+public class PanelCapturaEmpleados extends JPanel {
+
+	public PanelCapturaEmpleados(Service<Usuario> usuarios) {
+	}
+
+	public Component getLastItem() {
+		return null;
+	}
+
+	public void guardarEmpleado() {
+	}
+
+}
